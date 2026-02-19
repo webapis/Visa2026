54773242d5fb1f77b5e9f21cf9bc7e7e4a9b6baf
webapis.github <webapis.github@gmail.com>
2026-02-19 11:43:13 +0500

    Update ContractTemplate.cs

--- Visa2026/Visa2026.Module/BusinessObjects/ContractTemplate.cs
+++ Visa2026/Visa2026.Module/BusinessObjects/ContractTemplate.cs
@@ -1,6 +1,26 @@
+
+
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using DevExpress.ExpressApp.ConditionalAppearance;
+using DevExpress.ExpressApp.DC;
+using DevExpress.ExpressApp.Editors;
+using DevExpress.ExpressApp.Model;
 using DevExpress.Persistent.Base;
 using DevExpress.Persistent.BaseImpl.EF;
-using System.ComponentModel;
+using DevExpress.Persistent.Validation;
+using System.Linq;
+
+
+
+
+
+
+
 
 namespace Visa2026.Module.BusinessObjects
 {
@@ -10,6 +30,8 @@ namespace Visa2026.Module.BusinessObjects
     {
         public virtual string TemplateName { get; set; }
 
+       [FieldSize(FieldSizeAttribute.Unlimited)]
+        [EditorAlias("RichText")]
         public virtual string Content { get; set; }
     }
 }
