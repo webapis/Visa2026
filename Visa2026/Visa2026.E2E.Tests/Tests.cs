270b9a864068fd94ed8754502843f6d0e30a5095
webapis.github <webapis.github@gmail.com>
2026-02-20 10:49:45 +0500

    Update Tests.cs

--- Visa2026/Visa2026.E2E.Tests/Tests.cs
+++ Visa2026/Visa2026.E2E.Tests/Tests.cs
@@ -1,54 +1,66 @@
-﻿using DevExpress.EasyTest.Framework;
-using Xunit;
+﻿using System;
 using System.Runtime.Versioning;
+using DevExpress.EasyTest.Framework;
+using Xunit;
 
 [assembly: CollectionBehavior(DisableTestParallelization = true)]
-
-// To run functional tests for ASP.NET Core Blazor XAF Applications,
-// install browser drivers: https://www.selenium.dev/documentation/getting_started/installing_browser_drivers/.
-//
-// Refer to the following article for more information: https://docs.devexpress.com/eXpressAppFramework/403852/
 
 namespace Visa2026.E2E.Tests
 {
     public class Visa2026Tests : IDisposable
     {
-        const string BlazorAppName = "Visa2026Blazor";
-        const string AppDBName = "Visa2026EasyTest";
-        EasyTestFixtureContext FixtureContext { get; } = new EasyTestFixtureContext();
+        private const string BlazorAppName = "Visa2026Blazor";
+        private const string AppDBName     = "Visa2026EasyTest";
+
+        private EasyTestFixtureContext FixtureContext { get; } = new EasyTestFixtureContext();
 
         [SupportedOSPlatform("windows")]
         public Visa2026Tests()
         {
+            // Test edilecek Blazor uygulamasını ve DB'yi kaydet
             FixtureContext.RegisterApplications(
-                new BlazorApplicationOptions(BlazorAppName, string.Format(@"{0}\..\..\..\..\Visa2026.Blazor.Server", Environment.CurrentDirectory))
+                new BlazorApplicationOptions(
+                    BlazorAppName,
+                    string.Format(@"{0}\..\..\..\..\Visa2026.Blazor.Server", Environment.CurrentDirectory)
+                )
             );
-            FixtureContext.RegisterDatabases(new DatabaseOptions(AppDBName, "Visa2026EasyTest", server: @"(localdb)\mssqllocaldb"));
+
+            FixtureContext.RegisterDatabases(
+                new DatabaseOptions(
+                    AppDBName,
+                    "Visa2026EasyTest",
+                    server: @"(localdb)\mssqllocaldb"
+                )
+            );
         }
+
         public void Dispose()
         {
             FixtureContext.CloseRunningApplications();
         }
+
+        // Basit duman testi: uygulama açılıyor mu?
         [Theory]
         [InlineData(BlazorAppName)]
         [SupportedOSPlatform("windows")]
-        public void TestBlazorApp(string applicationName)
+        public void TestBlazorApp_Opens(string applicationName)
         {
             FixtureContext.DropDB(AppDBName);
+
+            // v24.2: CreateApplicationContext => IApplicationContext döndürür
             var appContext = FixtureContext.CreateApplicationContext(applicationName);
+
+            // v24.2: RunApplication parametresizdir
             appContext.RunApplication();
+
+            // Buradan itibaren C# EasyTest API ile etkileşim:
+            // Örnek: Logon -> Tasks'e Navigate
+            appContext.GetForm().FillForm(("User Name", "Admin"));
+            appContext.GetAction("Log In").Execute();
+            appContext.Navigate("Tasks");
         }
 
-        [Theory]
-        [InlineData(BlazorAppName, "sample.ets")]
-        [SupportedOSPlatform("windows")]
-        public void TestBlazorAppWithEts(string applicationName, string etsFileName)
-        {
-            FixtureContext.DropDB(AppDBName);
-            var appContext = FixtureContext.CreateApplicationContext(applicationName);
-            appContext.RunApplication();
-            // Use the correct API: IApplicationContext.Application.ExecuteTestScript
-            appContext.Application.ExecuteTestScript(etsFileName);
-        }
+        // (İSTEĞE BAĞLI) Başka akışlar burada C# API ile eklenebilir
+        // Örn: kayıt ekleme, tablo kontrolü, validation, vs.
     }
 }
