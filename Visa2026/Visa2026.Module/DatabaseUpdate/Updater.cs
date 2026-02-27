5aa340c95ebed0076ebd5e14dcfc577406f1ea66
webapis.github <webapis.github@gmail.com>
2026-02-27 19:15:12 +0500

    Update Updater.cs

--- Visa2026/Visa2026.Module/DatabaseUpdate/Updater.cs
+++ Visa2026/Visa2026.Module/DatabaseUpdate/Updater.cs
@@ -53,7 +53,7 @@ namespace Visa2026.Module.DatabaseUpdate
             // // CreateProjectContracts();
             // // CreateSubcontractors();
             //  CreateRelationships();
-            // // CreateApplicationStates();
+             //CreateApplicationStates();
             //  CreateApplicationLocations();
             // SeedOrganizationAndApplicationTypes();
             // CreateApplicationReasons();
