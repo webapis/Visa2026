f9824794df4b83d045e1ce1148f4fe5c0448217a
webapis.github <webapis.github@gmail.com>
2026-03-05 10:52:13 +0500

    save

--- Visa2026/Visa2026.Module/BusinessObjects/SingleActiveBaseObject.cs
+++ Visa2026/Visa2026.Module/BusinessObjects/SingleActiveBaseObject.cs
@@ -5,17 +5,18 @@ using System.Linq;
 using System.ComponentModel.DataAnnotations.Schema;
 using DevExpress.ExpressApp;
 using DevExpress.ExpressApp.ConditionalAppearance;
+using DevExpress.Persistent.Base;
 using DevExpress.Persistent.BaseImpl.EF;
 
 namespace Visa2026.Module.BusinessObjects
 {
-    [Appearance("SingleActiveBaseObject_ReadOnlyWhenInactive", AppearanceItemType = "ViewItem", TargetItems = "*", Criteria = "!IsActive", Context = "DetailView", Enabled = false)]
     public abstract class SingleActiveBaseObject<TParent, TItem> : BaseObject, IObjectSpaceLink
         where TParent : BaseObject
         where TItem : SingleActiveBaseObject<TParent, TItem>
     {
         private bool isActive;
 
+        [ImmediatePostData]
         [Appearance("SingleActiveBaseObject_DisableUncheck", Enabled = false, Criteria = "IsActive")]
         public virtual bool IsActive
         {
