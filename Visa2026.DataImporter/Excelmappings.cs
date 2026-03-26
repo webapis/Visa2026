66245244c5b2e5ea2aa5aa10597ff516ca0cbf85
webapis.github <webapis.github@gmail.com>
2026-03-26 13:58:52 +0500

    save

--- Visa2026.DataImporter/Excelmappings.cs
+++ Visa2026.DataImporter/Excelmappings.cs
@@ -407,10 +407,8 @@ public static class ExcelMappings
             Columns = new() {
                 new() { Header = "Document Number",  PayloadProperty = "DocumentNumber",   Kind = ColumnKind.Scalar,        Required = true },
                 new() { Header = "Issue Date",       PayloadProperty = "IssueDate",        Kind = ColumnKind.Scalar,        Required = true },
-                new() { Header = "Expiration Date",  PayloadProperty = "ExpirationDate",   Kind = ColumnKind.Scalar },
-                new() { Header = "Is Active",        PayloadProperty = "IsActive",         Kind = ColumnKind.Scalar },
                 new() { Header = "Person",           PayloadProperty = "Person",           Kind = ColumnKind.PersonLookupByName, Required = true },
-                new() { Header = "Validity Duration",PayloadProperty = "ValidityDuration", Kind = ColumnKind.LookupByName,  LookupEntity = "ValidityDuration" },
+                new() { Header = "Validity Duration",PayloadProperty = "ValidityDuration", Kind = ColumnKind.LookupByName,  LookupEntity = "ValidityDuration", Required = true },
             }
         },
         new SheetMap { SheetName = "Registrations", EntityName = "Registration",   DisplayName = "Registration",
