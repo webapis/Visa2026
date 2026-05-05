804f79799486f10dd28639e9a09199ab1bd179ed
webapis <60083568+webapis@users.noreply.github.com>
2026-05-05 16:33:22 +0500

    hide employee conract

---
+++ droplet-scripts/update-prod.ps1
@@ -0,0 +1,10 @@
+# Visa2026 convenience wrapper: update PROD on droplet using the visa SSH key.
+
+$ErrorActionPreference = "Stop"
+
+$scriptDir = Split-Path -Parent $MyInvocation.MyCommand.Path
+Set-Location $scriptDir
+
+& .\update-app.ps1 -Environment prod -IdentityFile "C:\Users\webap\.ssh\id_ed25519_visa"
+if ($LASTEXITCODE -ne 0) { exit $LASTEXITCODE }
+
