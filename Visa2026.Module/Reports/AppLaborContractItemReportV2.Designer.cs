b566be2b19c55252b8af67eabcb8bea8a27cd134
webapis <60083568+webapis@users.noreply.github.com>
2026-05-05 11:24:53 +0500

    Increase inter-section spacing to 26F for better vertical rhythm

--- Visa2026.Module/Reports/AppLaborContractItemReportV2.Designer.cs
+++ Visa2026.Module/Reports/AppLaborContractItemReportV2.Designer.cs
@@ -162,11 +162,11 @@ using DevExpress.XtraReports.UI;
                 new ExpressionBinding("BeforePrint", "Text", "FormatString(\"\\\"\\u00C7alyk Enerji Senagat we Tijaret A.\\u015E.\\\" T\\u00FCrk k\\u00E4rhanasyny\\u0148 T\\u00FCrkmenistandaky \\u015Faham\\u00E7asyny\\u0148 M\\u00FAdiri {0} bilen mundan be\\u00FDl\\u00E4k \\\"I\\u015FG\\u00C4R\\\" di\\u00FDip atlandyryl\\u00FDan {1} arasynda z\\u00E4hmet \\u015Fertnamasy bagla\\u015Fyldy. I\\u015FG\\u00C4R {2} wezipesine i\\u015Fe kabul edil\\u00FD\\u00E4r.\", [Application_SponsorSignatory], [Person_FullName], [Position_PositionTm])")
             });
             this.tableCellIntro.Multiline = true;
-            this.tableCellIntro.Padding = new PaddingInfo(0, 0, 0, 0, 100F);
+            this.tableCellIntro.Padding = new PaddingInfo(0F, 0F, 0F, 4F, 100F);
             this.tableCellIntro.TextAlignment = TextAlignment.TopJustify;
 
             this.tableRowSpacer1.Cells.AddRange(new XRTableCell[] { this.tableCellSpacer1 });
-            this.tableRowSpacer1.HeightF = 20F;
+            this.tableRowSpacer1.HeightF = 26F;
             this.tableCellSpacer1.Text = "";
 
             this.tableRowSection1Header.Cells.AddRange(new XRTableCell[] { this.tableCellSection1Header });
@@ -178,12 +178,12 @@ using DevExpress.XtraReports.UI;
             this.tableRowSection1Body.Cells.AddRange(new XRTableCell[] { this.tableCellSection1Body });
             this.tableRowSection1Body.HeightF = 96F;
             this.tableCellSection1Body.Multiline = true;
-            this.tableCellSection1Body.Padding = new PaddingInfo(20F, 0, 0, 0, 100F);
+            this.tableCellSection1Body.Padding = new PaddingInfo(20F, 0F, 0F, 4F, 100F);
             this.tableCellSection1Body.Text = "1.1. H\u00fcn\u00e4rine g\u00f6r\u00e4 i\u015f bilen \u00fcpj\u00fcn etmelidir.\r\n1.2. Her a\u00fd a\u00fdlyk z\u00e4hmet hakyny bellenilen g\u00fcnde t\u00f6lemelidir.\r\n1.3. Hereket ed\u00fd\u00e4n T\u00fcrkmenistany\u0148 Z\u00e4hmet baradaky kanunlar kodeksine la\u00fdklykda kesgitlenen m\u00f6hletde \u00fdyllyk z\u00e4hmet rugsadyny bermelidir.\r\n1.4. \u015eertnamany\u0148 m\u00f6hleti boyunca hereket ed\u00fd\u00e4n T\u00fcrkmenistany\u0148 Z\u00e4hmet baradaky kanunlar kodeksine la\u00fdklykda i\u015f \u00fc\u00e7in orna\u015fdyrylan d\u00fcz\u00fcmli, sosial goraglary we be\u00fdleki kepillikleri bermelidir.";
             this.tableCellSection1Body.TextAlignment = TextAlignment.TopJustify;
 
             this.tableRowSpacer2.Cells.AddRange(new XRTableCell[] { this.tableCellSpacer2 });
-            this.tableRowSpacer2.HeightF = 20F;
+            this.tableRowSpacer2.HeightF = 26F;
             this.tableCellSpacer2.Text = "";
 
             this.tableRowSection2Header.Cells.AddRange(new XRTableCell[] { this.tableCellSection2Header });
@@ -195,12 +195,12 @@ using DevExpress.XtraReports.UI;
             this.tableRowSection2Body.Cells.AddRange(new XRTableCell[] { this.tableCellSection2Body });
             this.tableRowSection2Body.HeightF = 118F;
             this.tableCellSection2Body.Multiline = true;
-            this.tableCellSection2Body.Padding = new PaddingInfo(20F, 0, 0, 0, 100F);
+            this.tableCellSection2Body.Padding = new PaddingInfo(20F, 0F, 0F, 4F, 100F);
             this.tableCellSection2Body.Text = "2.1. Bu \u015fertnama bo\u00fdun\u00e7a tab\u015fyrylan i\u015fleri dogry we doly \u00fdyerine \u00fdyetirmeli.\r\n2.2. K\u00e4rhanadaky i\u00e7erki d\u00fczg\u00fcn, tehniki howpsuzlyk hem-de \u00f6n\u00fcm\u00e7ilik tertibine la\u00fdk bolmaly, z\u00e4hmet howpsuzlygy we z\u00e4hmeti goramak d\u00fczg\u00fcnlerini berja\u00fd etmeli.\r\n2.3. I\u015f \u00fdyerini arassa saklamaly we enjamlary \u00e4ti\u00fda\u00e7lylyk bilen ulanmaly.\r\n2.4. Kompani\u00fdany\u0148 hyzmat syrlaryny gizlin saklamaly.\r\n2.5. B\u00f6l\u00fcn ba\u015flygyny\u0148 g\u00f6rkezmelerini \u00fdyerine \u00fdyetirmeli.";
             this.tableCellSection2Body.TextAlignment = TextAlignment.TopJustify;
 
             this.tableRowSpacer3.Cells.AddRange(new XRTableCell[] { this.tableCellSpacer3 });
-            this.tableRowSpacer3.HeightF = 20F;
+            this.tableRowSpacer3.HeightF = 26F;
             this.tableCellSpacer3.Text = "";
 
             this.tableRowSection3Header.Cells.AddRange(new XRTableCell[] { this.tableCellSection3Header });
@@ -212,12 +212,12 @@ using DevExpress.XtraReports.UI;
             this.tableRowSection3Body.Cells.AddRange(new XRTableCell[] { this.tableCellSection3Body });
             this.tableRowSection3Body.HeightF = 90F;
             this.tableCellSection3Body.Multiline = true;
-            this.tableCellSection3Body.Padding = new PaddingInfo(20F, 0, 0, 0, 100F);
+            this.tableCellSection3Body.Padding = new PaddingInfo(20F, 0F, 0F, 4F, 100F);
             this.tableCellSection3Body.Text = "3.1. I\u015f tertibine we T\u00fcrkmenistany\u0148 Z\u00e4hmet baradaky kanunlar kodeksine la\u00fdk bolmaly.\r\n3.2. I\u015f wagty \u2014 g\u00fcnde 8 sagat, hepdede 6 g\u00fcn.\r\n3.3. Zerur bolanda kanun\u00e7ylykdaky tertipde go\u015fma\u00e7a i\u015fe \u00e7agyrylyp bilin\u00fd\u00e4r.\r\n3.4. A\u00fdlyk z\u00e4hmet haky kadr b\u00f6l\u00fcn\u00fd\u0148 sanawyny\u0148 esasynda t\u00f6len\u00fd\u00e4r.";
             this.tableCellSection3Body.TextAlignment = TextAlignment.TopJustify;
 
             this.tableRowSpacer4.Cells.AddRange(new XRTableCell[] { this.tableCellSpacer4 });
-            this.tableRowSpacer4.HeightF = 20F;
+            this.tableRowSpacer4.HeightF = 26F;
             this.tableCellSpacer4.Text = "";
 
             this.tableRowSection4Header.Cells.AddRange(new XRTableCell[] { this.tableCellSection4Header });
@@ -229,12 +229,12 @@ using DevExpress.XtraReports.UI;
             this.tableRowSection4Body.Cells.AddRange(new XRTableCell[] { this.tableCellSection4Body });
             this.tableRowSection4Body.HeightF = 162F;
             this.tableCellSection4Body.Multiline = true;
-            this.tableCellSection4Body.Padding = new PaddingInfo(20F, 0F, 0F, 0F, 100F);
+            this.tableCellSection4Body.Padding = new PaddingInfo(20F, 0F, 0F, 4F, 100F);
             this.tableCellSection4Body.Text = "Z\u00e4hmet \u015fertnamasy \"I\u015f BERIJI\" tarapyndan a\u015fakdaky \u00fdagda\u00fdlarda \u00fdatyryl\u00fd\u00e4r:\r\n4.1. Z\u00e4hmet \u015fertnamasyny\u0148 m\u00f6hletini\u0148 gutarmagy;\r\n4.2. I\u015fleri\u0148 gutarmagy;\r\n4.3. I\u015fg\u00e4ri\u0148 azalmagy;\r\n4.4. I\u015fe serho\u015f bolup, narkotiki \u00fdya-da z\u00e4herli maddalary\u0148 t\u00e4siri astynda gelmegi;\r\n4.5. Z\u00e4hmet \u015fertnamasyna \u00fdya-da k\u00e4rhanany\u0148 i\u00e7erki tertip d\u00fczg\u00fcnlerine la\u00fdklykda \u00f6z\u00fcn\u00e9 tab\u015fyrylan bor\u00e7lary i\u015fg\u00e4ri\u0148 berja\u00fd etm\u00e4n \u00fdyeri\u0148e \u00fdyetirmezligi;\r\n4.6. Kompani\u00fdany\u0148 eml\u00e4gine zeper \u00fdetirende \u00fdya-da ogurlanda;\r\n4.7. Galan dawalar T\u00fcrkmenistany\u0148 hereket ed\u00fd\u00e4n kanunlaryna la\u00fdk \u00e7\u00f6z\u00fcl\u00fd\u00e4r.";
             this.tableCellSection4Body.TextAlignment = TextAlignment.TopJustify;
 
             this.tableRowSpacer5.Cells.AddRange(new XRTableCell[] { this.tableCellSpacer5 });
-            this.tableRowSpacer5.HeightF = 20F;
+            this.tableRowSpacer5.HeightF = 26F;
             this.tableCellSpacer5.Text = "";
 
             this.tableRowSection5Header.Cells.AddRange(new XRTableCell[] { this.tableCellSection5Header });
@@ -249,16 +249,18 @@ using DevExpress.XtraReports.UI;
             {
                 new ExpressionBinding("BeforePrint", "Text", "FormatString('Z\u00e4hmet \u015fertnamasy {0} \u2014 {1} aralygynda hereket ed\u00fd\u00e4r.', [Contract_StartDateText], [Contract_ExpirationDateText])")
             });
+            this.tableCellSection5Line1.Padding = new PaddingInfo(0F, 0F, 0F, 4F, 100F);
             this.tableCellSection5Line1.TextAlignment = TextAlignment.TopLeft;
 
             this.tableRowSection5Line2.Cells.AddRange(new XRTableCell[] { this.tableCellSection5Line2 });
             this.tableRowSection5Line2.HeightF = 46F;
             this.tableCellSection5Line2.Multiline = true;
-            this.tableCellSection5Line2.Text = "\u015eertnama Taraplar gol \u00E7ekenlerinden so\u0148 g\u00FC\u00FDje gir\u00FD\u00E4r. M\u00F6hleti gutaran \u00FDagda\u00FDynda, eger i\u015F gatna\u015Fyklaryny\u0148 hakykaty \u00FD\u00FCz\u00FCnde dowam ed\u00FD\u00E4n bolsa \u00FDya taraplary\u0148 hi\u00E7 biri \u015Fertnamany \u00FDatyrmak barada ikinji tarapa \u00FD\u00Fcz tutmasa, \u015Fertnama ilkinji bagla\u015Fylan g\u00F6rkezilen m\u00F6hlet m\u00F6\u00E7berinde uzadylan hasap edil\u00FD\u00E4r.";
+            this.tableCellSection5Line2.Padding = new PaddingInfo(0F, 0F, 0F, 4F, 100F);
+            this.tableCellSection5Line2.Text = "\u015eertnama Taraplar gol \u00E7ekenlerinden so\u0148 g\u00FC\u00YDje gir\u00YD\u00E4r. M\u00F6hleti gutaran \u00YDagda\u00FDynda, eger i\u015F gatna\u015Fyklaryny\u0148 hakykaty \u00YD\u00FCz\u00FCnde dowam ed\u00YD\u00E4n bolsa \u00FDya taraplary\u0148 hi\u00E7 biri \u015Fertnamany \u00FDatyrmak barada ikinji tarapa \u00FD\u00Fcz tutmasa, \u015Fertnama ilkinji bagla\u015Fylan g\u00F6rkezilen m\u00F6hlet m\u00F6\u00E7berinde uzadylan hasap edil\u00YD\u00E4r.";
             this.tableCellSection5Line2.TextAlignment = TextAlignment.TopLeft;
 
             this.tableRowSpacer6.Cells.AddRange(new XRTableCell[] { this.tableCellSpacer6 });
-            this.tableRowSpacer6.HeightF = 20F;
+            this.tableRowSpacer6.HeightF = 26F;
             this.tableCellSpacer6.Text = "";
 
             this.tableRowSection6Header.Cells.AddRange(new XRTableCell[] { this.tableCellSection6Header });
@@ -271,17 +273,19 @@ using DevExpress.XtraReports.UI;
             this.tableRowSection6Line1.HeightF = 26F;
             this.tableCellSection6Line1.ExpressionBindings.AddRange(new ExpressionBinding[]
             {
-                new ExpressionBinding("BeforePrint", "Text", "FormatString('A\u00fdlyk z\u00E4hmet haky: {0} {1}.', [Contract_SalaryText], [Salary_CurrencyCode])")
+                new ExpressionBinding("BeforePrint", "Text", "FormatString('A\u00fdlyk z\u00e4hmet haky: {0} {1}.', [Contract_SalaryText], [Salary_CurrencyCode])")
             });
+            this.tableCellSection6Line1.Padding = new PaddingInfo(0F, 0F, 0F, 4F, 100F);
             this.tableCellSection6Line1.TextAlignment = TextAlignment.TopLeft;
 
             this.tableRowSection6Line2.Cells.AddRange(new XRTableCell[] { this.tableCellSection6Line2 });
             this.tableRowSection6Line2.HeightF = 26F;
-            this.tableCellSection6Line2.Text = "T\u00f6leg: z\u00E4hmet haky T\u00fcrki\u00fdyed\u00e4ki bank hasabyna ge\u00e7iril\u00fd\u00e4r.";
+            this.tableCellSection6Line2.Padding = new PaddingInfo(0F, 0F, 0F, 4F, 100F);
+            this.tableCellSection6Line2.Text = "T\u00f6leg: z\u00e4hmet haky T\u00fcrki\u00fdyed\u00e4ki bank hasabyna ge\u00e7iril\u00fd\u00e4r.";
             this.tableCellSection6Line2.TextAlignment = TextAlignment.TopLeft;
 
             this.tableRowSpacer7.Cells.AddRange(new XRTableCell[] { this.tableCellSpacer7 });
-            this.tableRowSpacer7.HeightF = 20F;
+            this.tableRowSpacer7.HeightF = 26F;
             this.tableCellSpacer7.Text = "";
 
             this.tableRowSection7Header.Cells.AddRange(new XRTableCell[] { this.tableCellSection7Header });
@@ -291,7 +295,7 @@ using DevExpress.XtraReports.UI;
             this.tableCellSection7Header.TextAlignment = TextAlignment.TopLeft;
 
             this.tableRowSpacer8.Cells.AddRange(new XRTableCell[] { this.tableCellSpacer8 });
-            this.tableRowSpacer8.HeightF = 20F;
+            this.tableRowSpacer8.HeightF = 26F;
             this.tableCellSpacer8.Text = "";
 
             this.tableRowSignatures.Cells.AddRange(new XRTableCell[] { this.tableCellSignatures });
