23119ff2d19abeb6379365478d7196ac38cd4326
webapis <60083568+webapis@users.noreply.github.com>
2026-05-22 09:17:10 +0500

    Create _culture_test.cs

---
+++ _culture_test.cs
@@ -0,0 +1,6 @@
+﻿using System;
+using System.Globalization;
+foreach (var n in new[] { "en-US", "tr-TR", "tk-TM", "ru-RU", "tk" }) {
+  try { Console.WriteLine(n + " OK " + CultureInfo.GetCultureInfo(n).Name); }
+  catch (Exception ex) { Console.WriteLine(n + " FAIL " + ex.Message); }
+}
